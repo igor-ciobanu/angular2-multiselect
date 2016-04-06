7c237340d47414ca2ccfc52fd8b2fb65fcd14fa0
iciobanu <igor.ciobanu@yopeso.com>
2016-04-06 16:22:04 +0300

    change structure

--- component/src/multiselect.ts
+++ component/src/multiselect.ts
@@ -1,130 +1,262 @@
-import {Component, Input, Output, EventEmitter} from 'angular2/core';
-import {Pipe, PipeTransform} from 'angular2/core';
+import {
+    Component, Input, Output, Injectable, Pipe, EventEmitter, ElementRef, OnInit, PipeTransform, OnChanges, SimpleChange
+} from 'angular2/core';
 
-@Pipe({
-    name: "searchText"
-})
+const MULTISELECT_TEMPLATE =  `
+         <input type="button" class="pt-multiselect-trigger empty-val" [class.empty-val]="isModelEmpty()" value="{{multiselectHeader}}" (click)="toggleSelect()">
+         <div class="pt-multiselect-menu-wr" *ngIf="isOpen()" [ngStyle]="{'display': toggleSelectState}">
+            <div class="pt-multiselect-menu">
+                <div class="pt-multiselect-header">
+                    <div class="pt-input-field">
+                        <input [(ngModel)]="filterModel" class="pt-input" type="search" autofocus="autofocus" placeholder="Filter">
+                    </div>
+                    <div *ngIf="isMultiple()" class="pt-multiselect-actions">
+                        <a (click)="checkAll()" class="pt-multiselect-action">
+                            <svg class="action-icon">
+                                <use xlink:href="/src/dest/sprite/multiselect.svg#check_all"></use>
+                            </svg>
+                            Check all
+                        </a>
+                        <a (click)="unCheckAll()" class="pt-multiselect-action">
+                            <svg class="action-icon">
+                                <use xlink:href="/src/dest/sprite/multiselect.svg#uncheck_all"></use>
+                            </svg>
+                            Uncheck all
+                        </a>
+                    </div>
+                </div>
+                <ul class="pt-multiselect-items">
+                    <li class="pt-multiselect-item" *ngFor="#item of collection | searchText: filterModel: label" (click)="toggleSelectItem(item)">
+                        <i class="pt-checkbox" [class.checked]="isActive(item)"></i>
+                        <label class="pt-multiselect-item-label">{{item[label]}}</label>
+                    </li>
+                </ul>
+            </div>
+          </div>`
 
-class SearchTextPipe implements PipeTransform{
+@Pipe({name: "searchText"})
+class SearchTextPipe implements PipeTransform {
     transform(items: any, args:string[]) {
         let [searchKey, label] = args;
         if (searchKey == '' || searchKey == undefined) {
             return items;
         }
-        return items.filter((item: any) => item[label].indexOf(searchKey) !== -1);
+        return items.filter((item: any) => item[label].toLowerCase().indexOf(searchKey.toLowerCase()) !== -1);
     }
 }
 
-const MULTISELECT_TEMPLATE = `
-    <div class="btn-group">
-        <button class="btn btn-default dropdown-toggle" (click)="toggleSelect()">
-            {{multiselectHeader}}<span class="caret"></span>
-        </button>
-        <div class="dropdown-menu" [ngStyle]="{'display': toggleSelectState}">
-            <div class="dropdown-header">
-                <div>
-                    <input [(ngModel)]="filterModel"
-                           class="form-control input-sm" type="text"  autofocus="autofocus" placeholder="Filter">
-                </div>
-                <div *ngIf="multiple">
-                    <button (click)="checkAll()" class="btn btn-link btn-xs">
-                        <i class="glyphicon glyphicon-ok"></i>Check all
-                    </button>
-                    <button (click)="unCheckAll()" class="btn btn-link btn-xs">
-                        <i class="glyphicon glyphicon-remove"></i>Uncheck all
-                    </button>
-                </div>
-            </div>
-            <ul>
-                <li *ngFor="#item of collection | searchText: filterModel: label">
-                    <a class="checked" (click)="selectItem(item)"><i class="glyphicon"></i>{{item[label]}}</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-`;
-
+@Injectable()
 @Component({
+    template: MULTISELECT_TEMPLATE,
     selector: 'multiselect',
     pipes: [SearchTextPipe],
-    template: MULTISELECT_TEMPLATE,
+    host: {
+        "(document: click)": "handleDocClickEvent($event)",
+        "(click)": "trackClickEvent($event)"
+    }
 })
-
-export class Multiselect {
+export class Multiselect implements OnInit, OnChanges {
 
     @Input('collection') collection: any;
     @Input('multiple') multiple: boolean;
     @Input('label') label: string;
-    @Input('header') header: string;
+    @Input('trackBy') trackBy: string;
+    @Input('header') header: string = 'Select';
     @Input('mutiselectModel') mutiselectModel: any;
-    toggleSelectState: string = 'none';
-    multiselectHeader: string = '';
 
-    @Output()
-    modelUpdated = new EventEmitter<any>();
+    @Output() modelUpdated = new EventEmitter<any>();
+
+    itemStatus: any = {
+        selected: {},
+        unselected: {}
+    };
+    multiselectHeader: string = '';
+    toggleSelectState: string = 'none';
+    hostEvent: any = null;
+    states: any = {
+        open: false
+    };
+
+    constructor(private elementRef: ElementRef) {
+        this.elementRef = elementRef;
+    }
 
     ngOnInit() {
-        this.multiselectHeader = this.header || 'Select'
+        this.multiselectHeader = this.header || 'Select';
+    }
+
+    ngOnChanges(changes: {[key: string]: SimpleChange}) {
+        if (changes['collection'] &&
+            changes['collection'].currentValue !== undefined) {
+            this.outerCollectionUpdate();
+        }
+        if (this.collection !== undefined &&
+            changes['mutiselectModel'] &&
+            changes['mutiselectModel'].currentValue !== undefined) {
+            this.outerModelUpdate();
+        }
+    }
+
+    outerCollectionUpdate() {
+        for (var i = 0, len = this.collection.length; i < len; i++) {
+            let keyVal = i;
+            if (!this.isTrackByIndex()) {
+                keyVal = this.collection[i][this.trackBy];
+                if (keyVal === undefined) {
+                    throw new Error("Item doesn't have such [trackBy] key");
+                }
+                if (this.itemStatus.unselected[keyVal] !== undefined) {
+                    throw new Error("Collection contains duplication using [trackBy] key");
+                }
+            } else {
+                this.collection[i].$$index = keyVal;
+            }
+            this.itemStatus.unselected[keyVal] = true;
+        }
+    }
+
+    outerModelUpdate() {
+        if (this.isMultiple()) {
+            //@todo for multiselect;
+            this.updateMultipleHeader();
+        } else {
+            let key = this.getTrackByKey(this.mutiselectModel);
+            if (key !== undefined) {
+                this.check(key);
+            } else {
+                for (let i = 0, keys = Object.keys(this.itemStatus.selected), len = keys.length; i < len; i++) {
+                    this.unCheck(keys[i]);
+                }
+            }
+            this.updateSimpleHeader();
+        }
+    }
+
+    innerModelUpdate() {
+        if (this.isMultiple()) {
+            this.mutiselectModel = [];
+        }
+        for (let item of this.collection) {
+            if (this.isChecked(this.getTrackByKey(item))) {
+                if (!this.isMultiple()) {
+                    this.mutiselectModel = item;
+                    break;
+                }
+                this.mutiselectModel.push(item);
+            }
+        }
+        if (this.isMultiple()) {
+            this.updateMultipleHeader();
+        } else {
+            this.updateSimpleHeader();
+        }
+        this.modelUpdated.emit(this.mutiselectModel);
     }
 
     toggleSelect() {
-        if (this.toggleSelectState == 'none') {
-           this.toggleSelectState = 'block';
+        if (!this.isOpen()) {
+            this.states.open = true;
+            this.toggleSelectState = 'block';
         } else {
-           this.toggleSelectState = 'none';
+            this.states.open = false;
+            this.toggleSelectState = 'none';
         }
+    }
+
+    isOpen() {
+        return this.states.open;
+    }
+
+    isModelEmpty() {
+        if (this.isMultiple()) {
+            return this.mutiselectModel === undefined || this.mutiselectModel.length === 0;
+        }
+        return this.mutiselectModel === undefined || Object.keys(this.mutiselectModel).length === 0;
+    }
+
+    isTrackByIndex() {
+        return this.trackBy === undefined || this.trackBy.trim() === '';
+    }
+
+    check(key: any) {
+        if (this.isChecked(key)) {
+            return;
+        }
+        delete this.itemStatus.unselected[key];
+        this.itemStatus.selected[key] = true;
+        if (!this.isMultiple()) {
+            for (let i = 0, keys = Object.keys(this.itemStatus.selected), len = keys.length; i < len; i++) {
+                key != keys[i] && this.unCheck(keys[i]);
+            }
+        }
+    }
+
+    unCheck(key: any) {
+        delete this.itemStatus.selected[key];
+        this.itemStatus.unselected[key] = true;
+    }
+
+    getTrackByKey(item: any) {
+        return this.isTrackByIndex() ? item.$$index : item[this.trackBy];
+    }
+
+    isMultiple() {
+        return this.multiple;
+    }
+
+    handleDocClickEvent(event: any) {
+        this.hostEvent !== event && this.isOpen() && this.toggleSelect();
+    }
+
+    trackClickEvent(event: any) {
+        this.hostEvent = event;
+    }
+
+    isChecked(key: any) {
+        return this.itemStatus.selected[key] === true;
+    }
+
+    isActive(item: any) {
+        return this.isOpen() && this.isChecked(this.getTrackByKey(item));
     }
 
     checkAll() {
-        if (this.multiple != true) {
-            return;
+        for (let i = 0, keys = Object.keys(this.itemStatus.unselected), len = keys.length; i < len; i++) {
+            this.check(keys[i]);
         }
-        this.collection.forEach((t: any) => t.checked = true);
-        this.updateMultipleModel();
+        this.innerModelUpdate();
     }
 
     unCheckAll() {
-        this.collection.forEach((t: any) => t.checked = false);
-        this.updateMultipleModel();
+        for (let i = 0, keys = Object.keys(this.itemStatus.selected), len = keys.length; i < len; i++) {
+            this.unCheck(keys[i]);
+        }
+        this.innerModelUpdate();
     }
 
-    selectItem(item: any) {
-        item.checked = !item.checked;
-        if (this.multiple != true) {
-            this.updateSingleModel(item);
+    toggleSelectItem(item: any) {
+        const itemKey = this.getTrackByKey(item);
+        if (this.isChecked(itemKey)) {
+            this.isMultiple() && this.unCheck(itemKey);
         } else {
-            this.updateMultipleModel();
+            this.check(itemKey);
         }
-    }
-
-    updateSingleModel(item: any) {
-        this.mutiselectModel = item;
-        this.updateSimpleHeader();
-        this.modelUpdated.emit(this.mutiselectModel);
-    }
-
-    updateMultipleModel() {
-        this.mutiselectModel = [];
-        for (let value of this.collection) {
-            if (value.checked) {
-                this.mutiselectModel.push(value);
-            }
-        }
-        this.updateMultipleHeader();
-        this.modelUpdated.emit(this.mutiselectModel);
-
+        this.innerModelUpdate();
     }
 
     updateMultipleHeader() {
         if (this.mutiselectModel.length > 0) {
-            this.multiselectHeader = this.mutiselectModel.length
+            if (this.mutiselectModel.length === 1) {
+                this.multiselectHeader = this.mutiselectModel[0][this.label];
+            } else {
+                this.multiselectHeader = 'Selected: ' + this.mutiselectModel.length;
+            }
         } else {
             this.multiselectHeader = this.header;
         }
-
     }
 
     updateSimpleHeader() {
-        this.multiselectHeader = this.mutiselectModel[this.label]
+        this.multiselectHeader = this.mutiselectModel[this.label] || this.header;
     }
 }
--- demo/app/bootstrap.ts
+++ demo/app/bootstrap.ts
@@ -1,3 +1,6 @@
+///<reference path="./../../node_modules/angular2/typings/browser.d.ts"/>ts
+
+
 import {bootstrap} from 'angular2/platform/browser';
 import {TaskStore} from './stores/tasklist';
 import Overview from './components/overview';
